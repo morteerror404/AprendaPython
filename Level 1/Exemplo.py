3d2147040817431dc931e0e35069d66d73e7ba13
morteerror404 <frotatv@gmail.com>
2025-04-10 22:11:41 -0300

    my first commit

---
+++ Level 1/Exemplo.py
@@ -0,0 +1,15 @@
+# Vamos abstrair uma carteira.
+
+nome = "carteira"
+quant_cartoes = 6
+valor_notas = 77
+valor_moedas = 1.25
+
+print("Temos uma ", nome, ", dentro dela temos", quant_cartoes, "cartões de crédito e débito" )
+
+print("Em outra partição existe dinheiro em espécie, uma nota de 50, outra de 20, uma de 5 e outra de 2 Reais.")
+print("Total em notas: ", valor_notas)
+
+print("Também temos moedas, pois acabei de comprar um punhado de balas sortidas, o vendedor me devolveu ", valor_moedas, " R$ em moedas.")
+
+
